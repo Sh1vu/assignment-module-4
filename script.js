0bbd794eecb8fed3128054988a2e9ec40c0bf913
pushpendra061 <pushpendra.singh2019@vitbhopal.ac.in>
2020-07-21 08:41:00 +0530

    second draft

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-// ******************************* 
-// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
-// Module 4 Assignment Instructions.
-//
-// The idea of this assignment is to take an existing array of names
-// and then output either Hello 'Name' or Good Bye 'Name' to the console.
-// The program should say "Hello" to any name except names that start with a "J"
-// or "j", otherwise, the program should say "Good Bye". So, the final output
-// on the console should look like this:
-/*
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-*/
-
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
 (function (window) {
-
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];})
+  // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+  // See Lecture 52, part 2
+  
+  
+  // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+  // the "speak" method and which you will expose to the global context
+  // See Lecture 52, part 1
+  var helloSpeaker = {};
+  
+  // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+  var speakWord = "Hello";
+  
+  // STEP 4: Rewrite the 'speak' function such that it is attached to the
+  // helloSpeaker object instead of being a standalone function.
+  // See Lecture 52, part 2
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  
+  // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+  // 'helloSpeaker' on the global scope as well.
+  // See Lecture 52, part 2
+  // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+  window.helloSpeaker = helloSpeaker;
+  
+  })(window);
